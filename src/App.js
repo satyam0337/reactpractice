37cc45ff81f648caddabc2d774162af857886c10
satyam kumar <satyam.masai123@gmail.com>
2023-10-26 00:50:36 +0530

    gta4

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import Get from "./crud/Get";
 import Allroutes from "./crud/Allroutes";
 import Counter from "./smallApp/Counter";
 import Navbar from "./crud/Navbar";
-
+import ReactGA from "react-ga4";
 function App() {
+  const TRACTING_ID = 'G-GNKR5KPJHR'
+  ReactGA.initialize(TRACTING_ID);
+  ReactGA.pageview(document.location.pathname)
   return (
     <div className="App">
       <Navbar />
